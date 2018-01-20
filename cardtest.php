71ff35f1e892376737a02be2994ebf1c57848641
maymunskoa <ivan.s@mail.bg>
2018-01-20 23:00:50 +0200

    Add files via upload

---
+++ cardtest.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Card Game test</title>
+	<link rel="stylesheet" href="css/cards.css">
+</head>
+<body>
+		<h1>This is my first card game test</h1>
+		<p>pick a card <span><button>pick</button></span></p>
+		<div id="fff">
+			<img alt="">
+		</div>
+
+	<p id="result"></p>
+	<script src="js/extract.js"></script>
+</body>
+</html>
