68abbdbcf16a39fc28b2207491763edf33996f3a
maymunskoa <ivan.s@mail.bg>
2018-01-21 02:29:27 +0200

    Add files via upload

--- cardtest.php
+++ cardtest.php
@@ -4,15 +4,39 @@
 	<meta charset="UTF-8">
 	<title>Card Game test</title>
 	<link rel="stylesheet" href="css/cards.css">
+	<link href="https://fonts.googleapis.com/css?family=Oswald:300,400&amp;subset=cyrillic" rel="stylesheet">
 </head>
 <body>
 		<h1>This is my first card game test</h1>
-		<p>pick a card <span><button>pick</button></span></p>
-		<div id="fff">
-			<img alt="">
+		<div>
+		<p>
+			player 1	
+		</p>
+		<button id="pone">pick</button>
+		
+		<div>
+
+			<img id="imgOne" alt="">
+		</div>
+			
+		<h4></h4>
+		<p id="score"></p>
+			
+<hr>
+			<div>
+		<p>
+			player 2	
+		</p>
+		<button id="ptwo">pick</button>
+			<div>
+				<img id="uimage"  alt="">
+			</div>	
+			<h4 id="uhf"></h4>
+			<p id="uscore"></p>
+			
 		</div>
 
-	<p id="result"></p>
+	<p id="uresult"></p>
 	<script src="js/extract.js"></script>
 </body>
 </html>
